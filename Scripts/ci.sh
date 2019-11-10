bcb077b4ca05319a2175eb0c9bada7b7a3abfbf2
Dan Federman <dfed@me.com>
2019-11-09 23:35:49 -0800

    Set up CI

---
+++ Scripts/ci.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+set -ex
+
+if [ $ACTION == "swift-package" ]; then
+  swift package generate-xcodeproj --output generated/
+  if [ -n "$DESTINATION" ]; then
+    xcodebuild -project generated/CacheAdvance.xcodeproj -scheme "CacheAdvance-Package" -sdk $SDK -destination "$DESTINATION" -configuration Release -PBXBuildsContinueAfterErrors=0 build
+  else
+    xcodebuild -project generated/CacheAdvance.xcodeproj -scheme "CacheAdvance-Package" -sdk $SDK -configuration Release -PBXBuildsContinueAfterErrors=0 build
+  fi
+fi
+
+if [ $ACTION == "pod-lint" ]; then
+  swift package generate-xcodeproj --output generated/
+  pushd generated/
+  bundle exec pod lib lint --verbose --fail-fast --swift-version=$SWIFT_VERSION
+  popd
+fi
+
+if [ $ACTION == "carthage" ]; then
+  swift package generate-xcodeproj --output generated/
+  pushd generated/
+  carthage build --verbose --no-skip-current
+  popd
+fi
