fd2c8a2d4be8e55c8d2d5ac90f28c629b397ce84
Dan Federman <dan.federman@airbnb.com>
2019-11-29 22:01:28 -0800

    Run tests in CI

--- Scripts/ci.sh
+++ Scripts/ci.sh
@@ -4,7 +4,7 @@ set -ex
 if [ $ACTION == "swift-package" ]; then
   swift package generate-xcodeproj --output generated/
   if [ -n "$DESTINATION" ]; then
-    xcodebuild -project generated/CacheAdvance.xcodeproj -scheme "CacheAdvance-Package" -sdk $SDK -destination "$DESTINATION" -configuration Release -PBXBuildsContinueAfterErrors=0 build
+    xcodebuild -project generated/CacheAdvance.xcodeproj -scheme "CacheAdvance-Package" -sdk $SDK -destination "$DESTINATION" -configuration Release -PBXBuildsContinueAfterErrors=0 build test
   else
     xcodebuild -project generated/CacheAdvance.xcodeproj -scheme "CacheAdvance-Package" -sdk $SDK -configuration Release -PBXBuildsContinueAfterErrors=0 build
   fi
