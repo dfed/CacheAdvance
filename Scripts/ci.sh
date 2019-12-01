9050828ece170258a30dbaeb0851a72dff46d346
Dan Federman <dan.federman@airbnb.com>
2019-12-01 09:43:05 -0800

    Inject BUILD parameter to CI script

--- Scripts/ci.sh
+++ Scripts/ci.sh
@@ -4,9 +4,22 @@ set -ex
 if [ $ACTION == "swift-package" ]; then
   swift package generate-xcodeproj --output generated/
   if [ -n "$DESTINATION" ]; then
-    xcodebuild -project generated/CacheAdvance.xcodeproj -scheme "CacheAdvance-Package" -sdk $SDK -destination "$DESTINATION" -configuration Release -PBXBuildsContinueAfterErrors=0 build test
+    xcodebuild \
+      -project generated/CacheAdvance.xcodeproj \
+      -scheme "CacheAdvance-Package" \
+      -sdk $SDK \
+      -destination "$DESTINATION" \
+      -configuration Release \
+      -PBXBuildsContinueAfterErrors=0 \
+      $BUILD
   else
-    xcodebuild -project generated/CacheAdvance.xcodeproj -scheme "CacheAdvance-Package" -sdk $SDK -configuration Release -PBXBuildsContinueAfterErrors=0 build
+    xcodebuild \
+      -project generated/CacheAdvance.xcodeproj \
+      -scheme "CacheAdvance-Package" \
+      -sdk $SDK \
+      -configuration Release \
+      -PBXBuildsContinueAfterErrors=0 \
+      $BUILD
   fi
 fi
 
