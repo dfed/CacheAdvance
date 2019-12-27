4b375888840b531a31045729fa4540f9120dfdc3
Dan Federman <dfed@me.com>
2019-12-27 13:50:57 -0800

    Update comment

--- Sources/CacheAdvance/FileHeader.swift
+++ Sources/CacheAdvance/FileHeader.swift
@@ -102,7 +102,7 @@ struct FileHeader {
 
     enum Field: Int, CaseIterable {
         // Header format:
-        // [headerVersion:UInt8][maximumBytes:UInt64][overwritesOldMessages:Bool][offsetInFileOfOldestMessage:UInt64][offsetInFileAtEndOfNewestMessage:UInt64]
+        // [headerVersion:UInt8][maximumBytes:UInt64][overwritesOldMessages:Bool][offsetInFileOfOldestMessage:UInt64][offsetInFileAtEndOfNewestMessage:UInt64][reservedSpace]
 
         case version = 0
         case maximumBytes
