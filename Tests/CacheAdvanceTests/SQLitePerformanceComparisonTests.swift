665584cf75fefaa29289c81a49751d8e20793a87
Dan Federman <dfed@me.com>
2020-04-26 12:45:55 -0700

    Make SQLitePerformanceComparisonTests use an object wrapper and enable fillable/overwriting cache semantics

--- Tests/CacheAdvanceTests/SQLitePerformanceComparisonTests.swift
+++ Tests/CacheAdvanceTests/SQLitePerformanceComparisonTests.swift
@@ -24,59 +24,156 @@ final class SQLitePerformanceComparisonTests: XCTestCase {
 
     // MARK: XCTestCase
 
-    override func tearDown() {
-        sqlite3_close(db)
+    override func setUp() {
+        super.setUp()
 
-        super.tearDown()
+        // Delete the existing cache.
+        try? FileManager.default.removeItem(at: testFileLocation)
+    }
+
+    // MARK: Behavior Tests
+
+    func test_append_sqlite_fillableCache_canMaintainMaxCount() {
+        let cache = SQLiteCache<TestableMessage>(
+            location: testFileLocation,
+            maxMessageCount: Int32(LorumIpsum.messages.count),
+            shouldOverwriteMessages: false)
+        for message in LorumIpsum.messages + LorumIpsum.messages {
+            cache.appendMessage(message)
+        }
+
+        XCTAssertEqual(cache.messages().count, LorumIpsum.messages.count)
+    }
+
+    func test_messages_sqlite_fillableCache_canReadInsertedMessages() {
+        let cache = SQLiteCache<TestableMessage>(
+            location: testFileLocation,
+            maxMessageCount: Int32(LorumIpsum.messages.count),
+            shouldOverwriteMessages: true)
+        for message in LorumIpsum.messages {
+            cache.appendMessage(message)
+        }
+
+        XCTAssertEqual(cache.messages(), LorumIpsum.messages)
+    }
+
+    func test_append_sqlite_overwritingCache_canMaintainMaxCount() {
+        let cache = SQLiteCache<TestableMessage>(
+            location: testFileLocation,
+            maxMessageCount: Int32(LorumIpsum.messages.count),
+            shouldOverwriteMessages: true)
+        for message in LorumIpsum.messages + LorumIpsum.messages {
+            cache.appendMessage(message)
+        }
+
+        XCTAssertEqual(cache.messages().count, LorumIpsum.messages.count)
+    }
+
+    func test_messages_sqlite_overwritingCache_canReadInsertedMessages() {
+        let cache = SQLiteCache<TestableMessage>(
+            location: testFileLocation,
+            maxMessageCount: Int32(LorumIpsum.messages.count),
+            shouldOverwriteMessages: true)
+        for message in LorumIpsum.messages {
+            cache.appendMessage(message)
+        }
+
+        XCTAssertEqual(cache.messages(), LorumIpsum.messages)
     }
 
     // MARK: Performance Tests
 
     func test_performance_createDatabaseAndAppendSingleMessage() {
         measure {
-            createDatabase()
-            insertMessage("test message")
+            // Delete any existing database.
+            try? FileManager.default.removeItem(at: testFileLocation)
+
+            let cache = SQLiteCache<TestableMessage>(
+                location: testFileLocation,
+                maxMessageCount: Int32(LorumIpsum.messages.count),
+                shouldOverwriteMessages: false)
+
+            cache.appendMessage("test message")
         }
     }
 
-    func test_performance_sqlite_append() {
-        createDatabase()
+    func test_performance_sqlite_append_fillableCache() {
+         // Create a cache that won't run out of room over multiple test runs.
+        let cache = SQLiteCache<TestableMessage>(
+            location: testFileLocation,
+            maxMessageCount: Int32(LorumIpsum.messages.count * 10),
+            shouldOverwriteMessages: false)
+        cache.createDatabaseIfNecessay()
+
         measure {
             for message in LorumIpsum.messages {
-                insertMessage(message)
+                cache.appendMessage(message)
+            }
+        }
+    }
+
+    func test_performance_sqlite_append_overwritingCache() {
+        let cache = SQLiteCache<TestableMessage>(
+            location: testFileLocation,
+            maxMessageCount: Int32(LorumIpsum.messages.count),
+            shouldOverwriteMessages: true)
+
+        // Fill the cache before the test starts.
+        for message in LorumIpsum.messages {
+            cache.appendMessage(message)
+        }
+        measure {
+            for message in LorumIpsum.messages {
+                cache.appendMessage(message)
             }
         }
     }
 
     func test_performance_sqlite_messages() {
-        createDatabase()
+        let cache = SQLiteCache<TestableMessage>(
+            location: testFileLocation,
+            maxMessageCount: Int32(LorumIpsum.messages.count),
+            shouldOverwriteMessages: false)
         for message in LorumIpsum.messages {
-            insertMessage(message)
+            cache.appendMessage(message)
         }
         measure {
-            let _ = readMessages()
+            let _ = cache.messages()
         }
-    }
-
-    // MARK: Behavior Tests
-
-    func test_readMessages_sqlite_canReadInsertedMessages() {
-        createDatabase()
-        for message in LorumIpsum.messages {
-            insertMessage(message)
-        }
-
-        XCTAssertEqual(readMessages(), LorumIpsum.messages)
     }
 
     // MARK: Private
 
-    func createDatabase() {
+    private let testFileLocation = FileManager.default.temporaryDirectory.appendingPathComponent("SQLiteTests")
+
+}
+
+class SQLiteCache<T: Codable> {
+
+    // MARK: Initialization
+
+    init(location: URL,
+         maxMessageCount: Int32,
+         shouldOverwriteMessages: Bool)
+    {
+        self.location = location
+        self.maxMessageCount = maxMessageCount
+        self.shouldOverwriteMessages = shouldOverwriteMessages
+    }
+
+    deinit {
         if let db = db {
             sqlite3_close(db)
         }
-        try? FileManager.default.removeItem(at: testFileLocation)
-        guard sqlite3_open(NSString(string: testFileLocation.path).utf8String, &db) == SQLITE_OK else {
+    }
+
+    // MARK: Internal
+
+    func createDatabaseIfNecessay() {
+        guard db == nil else {
+            return
+        }
+        guard sqlite3_open(NSString(string: location.path).utf8String, &db) == SQLITE_OK else {
             XCTFail("Failed to open database")
             return
         }
@@ -92,11 +189,34 @@ final class SQLitePerformanceComparisonTests: XCTestCase {
         sqlite3_finalize(createTableStatement)
     }
 
-    func insertMessage(_ message: TestableMessage) {
+    func appendMessage(_ message: T) {
+        createDatabaseIfNecessay()
         guard let messageData = try? encoder.encode(message),
             let messageDataPointer = messageData.withUnsafeBytes({ $0.baseAddress }) else {
             XCTFail("Could not encode message")
             return
+        }
+
+        if messageCount() == maxMessageCount {
+            guard shouldOverwriteMessages else {
+                // We do not have room for this message.
+                return
+            }
+            // Delete the first message to make room for this message.
+            let rawDeleteQuery = "DELETE FROM Messages ORDER BY rowid LIMIT 1;"
+            var deleteQuery: OpaquePointer?
+            guard sqlite3_prepare_v2(db, rawDeleteQuery, -1, &deleteQuery, nil) == SQLITE_OK else {
+                XCTFail("Failed to prepare delete")
+                return
+            }
+            defer {
+                sqlite3_finalize(deleteQuery)
+            }
+
+            guard sqlite3_step(deleteQuery) == SQLITE_DONE else {
+                XCTFail("Failed to delete top messages")
+                return
+            }
         }
 
         let insertStatementString = "INSERT INTO Messages(Message) VALUES (?);"
@@ -114,7 +234,8 @@ final class SQLitePerformanceComparisonTests: XCTestCase {
         sqlite3_finalize(insertStatement)
     }
 
-    func readMessages() -> [TestableMessage] {
+    func messages() -> [T] {
+        createDatabaseIfNecessay()
         let rawQuery = "SELECT * FROM Messages;"
         var query: OpaquePointer?
         guard sqlite3_prepare_v2(db, rawQuery, -1, &query, nil) == SQLITE_OK else {
@@ -124,7 +245,7 @@ final class SQLitePerformanceComparisonTests: XCTestCase {
         defer {
             sqlite3_finalize(query)
         }
-        var logs = [TestableMessage]()
+        var logs = [T]()
         while sqlite3_step(query) == SQLITE_ROW {
             guard let messageDataPointer = sqlite3_column_blob(query, 0) else {
                 XCTFail("Failed to retrieve message blob")
@@ -132,7 +253,7 @@ final class SQLitePerformanceComparisonTests: XCTestCase {
             }
             let messageLength = sqlite3_column_bytes(query, 0)
             let messageData = Data(bytes: messageDataPointer, count: Int(messageLength))
-            guard let message = try? decoder.decode(TestableMessage.self, from: messageData) else {
+            guard let message = try? decoder.decode(T.self, from: messageData) else {
                 XCTFail("Failed to decode message")
                 return []
             }
@@ -142,10 +263,32 @@ final class SQLitePerformanceComparisonTests: XCTestCase {
         return logs
     }
 
-    private var db: OpaquePointer?
+    // MARK: Private
 
-    private let testFileLocation = FileManager.default.temporaryDirectory.appendingPathComponent("SQLiteTests")
+    private var db: OpaquePointer?
+    private let location: URL
+    private let maxMessageCount: Int32
+    private let shouldOverwriteMessages: Bool
     private let encoder = JSONEncoder()
     private let decoder = JSONDecoder()
+
+    private func messageCount() -> Int32 {
+        let rawCountQuery = "SELECT COUNT(*) FROM Messages;"
+        var countQuery: OpaquePointer?
+        guard sqlite3_prepare_v2(db, rawCountQuery, -1, &countQuery, nil) == SQLITE_OK else {
+            XCTFail("Failed to prepare read")
+            return Int32.max
+        }
+        defer {
+            sqlite3_finalize(countQuery)
+        }
+
+        guard sqlite3_step(countQuery) == SQLITE_ROW else {
+            XCTFail("Failed to get persisted message count")
+            return Int32.max
+        }
+
+        return sqlite3_column_int(countQuery, 0)
+    }
 
 }
