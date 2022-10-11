89204268414ac285ca70534e0791d74d690668dc
Dan Federman <dfed@me.com>
2022-10-11 15:10:22 -0400

    Get codecov working again

--- Scripts/upload-coverage-reports.sh
+++ Scripts/upload-coverage-reports.sh
@@ -4,6 +4,6 @@ set -ex
 IFS=','; PLATFORMS=$(echo $1); unset IFS
 
 for PLATFORM in $PLATFORMS; do
-	bash <(curl -s https://codecov.io/bash) -J '^CacheAdvance$' -D .build/derivedData/$PLATFORM -t 8344b011-6b2a-4b3d-a573-eaf49684318e
-	bash <(curl -s https://codecov.io/bash) -J '^CADCacheAdvance$' -D .build/derivedData/$PLATFORM -t 8344b011-6b2a-4b3d-a573-eaf49684318e
+	bash <(curl -s https://codecov.io/bash) -J '^CacheAdvance(.framework)?$' -D .build/derivedData/$PLATFORM -t 8344b011-6b2a-4b3d-a573-eaf49684318e
+	bash <(curl -s https://codecov.io/bash) -J '^CADCacheAdvance(.framework)?$' -D .build/derivedData/$PLATFORM -t 8344b011-6b2a-4b3d-a573-eaf49684318e
 done
