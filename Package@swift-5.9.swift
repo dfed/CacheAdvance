c7596426abe8e0fb6ec669731e44b99e65c076e2
Dan Federman <dfed@me.com>
2024-01-10 20:09:46 -0800

    Drop support for old versions

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,6 +11,7 @@ let package = Package(
         .watchOS(.v6),
         .macOS(.v10_15),
         .macCatalyst(.v13),
+        .visionOS(.v1)
     ],
     products: [
         .library(
